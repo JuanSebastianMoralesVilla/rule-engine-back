50e76e9842aff9f77a4e67e95233787b31687640
zonitidus <zonitidus@gmail.com>
2022-12-20 03:53:32 -0500

    Enable cors

--- src/main/java/com/perficient/challenge/rulesengine/controller/RuleController.java
+++ src/main/java/com/perficient/challenge/rulesengine/controller/RuleController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.perficient.challenge.rulesengine.service.interfaces.RulesService;
 
 @RestController
 @RequestMapping("/api/rules")
+@CrossOrigin("*")
 public class RuleController {
 
 	@Autowired
